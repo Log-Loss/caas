0b9c3f234ab11e02269034203671eb0cf40ca3c0
qyh <imqyh@qq.com>
2018-01-05 03:24:45 +0800

    change port & add data

--- poc/src/main/java/caas/poc/config/DatabaseConfiguration.java
+++ poc/src/main/java/caas/poc/config/DatabaseConfiguration.java
@@ -10,6 +10,13 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Configuration
 public class DatabaseConfiguration {
@@ -26,18 +33,41 @@ public class DatabaseConfiguration {
     ModelService modelService;
 
     @EventListener(ApplicationReadyEvent.class)
-    public void config() {
+    public void config() throws IOException {
         userService.create("user@email.com", "password");
         userService.create("user1@email.com", "password1");
         userService.create("user2@email.com", "password2");
 
-        workspaceService.create(1, "workspace");
-        workspaceService.create(1, "workspace1");
+        workspaceService.create(1, "1workspace");
+        workspaceService.create(1, "1workspace1");
 
         workspaceService.create(2, "2workspace");
         workspaceService.create(2, "2workspace1");
 
+        modelService.create(1, "1model");
+        modelService.create(1, "1model1");
+        modelService.create(1, "1model2");
+        modelService.create(2, "2model");
+        modelService.create(2, "2model1");
+        modelService.create(2, "2model2");
+        modelService.create(3, "3model");
+        modelService.create(3, "3model1");
+        modelService.create(3, "3model2");
 
+        
+        Resource resource = new ClassPathResource("datasets/t10k-labels-idx1-ubyte");
+        byte[] content = Files.readAllBytes(Paths.get(resource.getFile().getPath()));
+        datasetService.create(1, "1dataset", content, false);
+        datasetService.create(1, "1dataset1", content, false);
+        datasetService.create(1, "1dataset2", content, false);
+        datasetService.create(2, "2dataset", content, false);
+        datasetService.create(2, "2dataset1", content, false);
+        datasetService.create(2, "2dataset2", content, false);
+
+        datasetService.create(0, "dataset", content, true);
+        datasetService.create(0, "dataset1", content, true);
+        datasetService.create(0, "dataset2", content, true);
+        datasetService.create(0, "dataset3", content, true);
 
     }
 }
